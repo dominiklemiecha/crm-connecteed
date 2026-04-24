2bbb4d962bdb43e5f9a5a0f2dcf0863bef53e2cb
Dominik Lemiecha <dominik.lemiecha25@gmail.com>
2026-04-24 12:23:20 +0200

    fix: hardcode dropSchema for reset

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -58,7 +58,7 @@ import { OutlookModule } from './modules/outlook/outlook.module';
           database: config.get<string>('DB_DATABASE', 'crm_connecteed'),
           autoLoadEntities: true,
           synchronize: envConfig.syncDatabase || process.env.FORCE_DB_SYNC === 'true',
-          dropSchema: process.env.FORCE_DB_DROP === 'true',
+          dropSchema: true,
           logging: envConfig.logging,
         };
       },
